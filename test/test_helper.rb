c3f0a3e1bae0ad021a02d344936e6fef10345a4f
Eric Boehs <eric@boehs.com>
2026-01-17 09:28:55 -0600

    Add code coverage with SimpleCov
    Adds optional test coverage reporting via SimpleCov. Run with
    COVERAGE=1 bundle exec rake test to generate a coverage report.
    Includes bin/coverage script for parsing and displaying results.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
+if ENV['COVERAGE']
+  require 'simplecov'
+  SimpleCov.start do
+    add_filter '/test/'
+    enable_coverage :branch
+  end
+end
+
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 
 require 'minitest/autorun'
